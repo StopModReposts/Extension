0c68d36ec34a4219f585bbe5acdc5a48149709b2
dada513 <dada513@protonmail.com>
2021-12-19 21:44:13 +0100

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { APIResponse, IllegalSite } from "./types";
+console.log("background loaded");
+let cachedSites: APIResponse = [];
+let ignoreList: string[] = [];
+type ExtMessageType =
+  | "get-sites-list"
+  | "tab-update"
+  | "save-blocked-site"
+  | "get-blocked-site"
+  | "add-to-ignore";
+interface ExtMessage {
+  type: ExtMessageType;
+  data: any;
+}
+
+let lastBlockedSite: IllegalSite | null = null;
+
+axios.get("https://api.stopmodreposts.org/sites.json").then((res) => {
+  cachedSites = res.data;
+});
+
+chrome.runtime.onMessage.addListener(
+  (message: ExtMessage, sender, sendResponse) => {
+    if (message.type === "get-sites-list") {
+      console.log("sending site list", cachedSites);
+      if (cachedSites[0])
+        return sendResponse(
+          cachedSites.filter((site) => !ignoreList.includes(site.domain))
+        );
+      else return sendResponse(null);
+    }
+    if (message.type === "tab-update") {
+      chrome.tabs.update({
+        url: message.data,
+      });
+      return sendResponse(null);
+    }
+    if (message.type === "save-blocked-site") {
+      lastBlockedSite = message.data;
+      return sendResponse(null);
+    }
+    if (message.type === "get-blocked-site") {
+      console.log(lastBlockedSite);
+      return sendResponse(lastBlockedSite);
+    }
+    if (message.type === "add-to-ignore") {
+      ignoreList.push(message.data.domain);
+      return sendResponse(null);
+    }
+  }
+);
---
+++ src/contentScript.ts
@@ -0,0 +1,33 @@
+// import { matchPattern } from "url-matcher";
+import { APIResponse } from "./types";
+
+chrome.runtime.sendMessage(
+  { type: "get-sites-list" },
+  (response: APIResponse | null) => {
+    console.log(response);
+    if (!response) return console.log("ERR TOO FAST");
+    let host = location.host;
+    if (host.startsWith("www.")) {
+      host = host.slice(4);
+    }
+
+    const sites = response;
+    const site = sites.find(
+      (site) => site.domain === host || site.domain.endsWith(`.${host}`)
+    );
+    if (!site) return; // Hooray, not a repost site!
+    const pathCorrect = site.path
+      ? location.pathname.startsWith(site.path)
+      : true;
+    if (!pathCorrect) return;
+    site.ext_redirFrom = location.href;
+    chrome.runtime.sendMessage({
+      type: "save-blocked-site",
+      data: site,
+    });
+    chrome.runtime.sendMessage({
+      type: "tab-update",
+      data: chrome.runtime.getURL("/html/alert.html"),
+    });
+  }
+);
